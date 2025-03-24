f18e22b479a4f1c4526f01a8d8e4671489d72cd2
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2025-03-24 07:11:53 +0330

    Initial commit

---
+++ playground/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# Request -> response
+# request handler
+# action
+
+
+def sayHello(request):
+    # Pull data from db
+    # Transform
+    # Send email
+    return HttpResponse("Hello World")
