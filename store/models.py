9eca68dcc8408ec38d945c46926b67b45de0da1b
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2025-03-27 19:01:15 +0330

    Add Customer Class

--- store/models.py
+++ store/models.py
@@ -9,3 +9,11 @@ class Product(models.Model):
     price = models.DecimalField()
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now_add=True)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.BigIntegerField(max_length=255)
+    birth_date = models.DateField(auto_now_add=True)
