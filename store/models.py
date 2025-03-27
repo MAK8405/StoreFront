a0e11aee81e08dbc8abffa5b43f2c09804426cb4
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2025-03-27 13:30:17 +0330

    Add Product Class

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField()
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now_add=True)
