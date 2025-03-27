1229091d33dbe6951bca995685101d3a67f70c91
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2025-03-27 20:09:05 +0330

    Add Membership

--- store/models.py
+++ store/models.py
@@ -12,8 +12,17 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, "Bronze"),
+        (MEMBERSHIP_SILVER, "Silver"),
+        (MEMBERSHIP_GOLD, "Gold"),
+    ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.BigIntegerField(max_length=255)
     birth_date = models.DateField(auto_now_add=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default="B")
