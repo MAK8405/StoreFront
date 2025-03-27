13132cc33ab1e505870f98abe6c0faa51c83ad5b
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2025-03-27 20:46:32 +0330

    Add Order Class

--- store/models.py
+++ store/models.py
@@ -26,3 +26,19 @@ class Customer(models.Model):
     phone = models.BigIntegerField(max_length=255)
     birth_date = models.DateField(auto_now_add=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default="B")
+
+
+class Order(models.Models):
+    PAYMENT_STATUS_PENDING = "P"
+    PAYMENT_STATUS_COMPLETE = "C"
+    PAYMENT_STATUS_FAILED = "F"
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, "Pending"),
+        (PAYMENT_STATUS_COMPLETE, "Complete"),
+        (PAYMENT_STATUS_FAILED, "Failed"),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING
+    )
