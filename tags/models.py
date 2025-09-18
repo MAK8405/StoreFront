3682093095866cd114e27f76117b50cb6205b470
MAK8405 <mahdiahmadi2005kf@gmail.com>
2025-09-18 23:53:07 +0330

    Start tags models.py

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    # What tag si applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product)
