920a5f85bfc4d483ad9d816d35b6825ee2d6f49b
MAK8405 <mahdiahmadi2005kf@gmail.com>
2025-09-23 18:46:04 +0330

    Add Generic Relationship

--- tags/models.py
+++ tags/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Tag(models.Model):
@@ -12,3 +13,5 @@ class TaggedItem(models.Model):
     # Type (product, video, article)
     # ID
     product = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
